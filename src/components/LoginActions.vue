78ef6b24d1fe1567f15410c2e467bdb6e2cb5592
burditt1533 <burditt1533@yahoo.com>
2024-05-26 23:15:10 -0500

    wip

--- src/components/LoginActions.vue
+++ src/components/LoginActions.vue
@@ -16,9 +16,13 @@
       params[decodeURIComponent(m[1])] = decodeURIComponent(m[2])
     }
 
+    console.log(params)
+
     if(Object.keys(params).length > 0) {
+      console.log( JSON.stringify(params))
       localStorage.setItem('authInfo', JSON.stringify(params))
     }
+
 
     window.history.pushState({}, document.title, '/')
     settings.setAuthInfo(JSON.parse(localStorage.getItem('authInfo')))
